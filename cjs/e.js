222b75f4bd9c00230497b3d91644c16a595f507b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-03-04 15:09:42 +0100

    espruino friendly hooks syntax

--- cjs/e.js
+++ cjs/e.js
@@ -1,24 +1,30 @@
 'use strict';
 /*! (c) Andrea Giammarchi - ISC */
-const {hooked, useRef, useState} = ((h, o, O, K) => ({
-  hooked(f) {
+const uhooks = ((h, o, O, K) => ({
+  hooked: f => {
     const k = {i: 0, s: []};
     return function H() {
-      const [p, r, e, v] = [h, o, O, K];
-      [h, o, O, K] = [H, this, arguments, k];
+      // const [p, r, e, v] = [h, o, O, K];
+      // [h, o, O, K] = [H, this, arguments, k];
+      const p = h, r = o, e = O, v = K;
+      h = H; o = this; O = arguments; K = k;
       K.i = 0;
       try { return f.apply(o, O); }
-      finally { [h, o, O, K] = [p, r, e, v]; }
+      finally {
+        h = p; o = r; O = e; K = v;
+        // [h, o, O, K] = [p, r, e, v];
+      }
     };
   },
-  useRef(current) {
-    const {i, s} = K;
-    if (i == s.length)
-      s.push({current});
-    return s[K.i++];
+  useRef: current => {
+    // const {i, s} = K;
+    if (K.i == K.s.length)
+      K.s.push({current: current});
+    return K.s[K.i++];
   },
-  useState(value) {
-    const [y, u, p, {i, s}] = [h, o, O, K];
+  useState: value => {
+    // const [y, u, p, {i, s}] = [h, o, O, K];
+    const y = h, u = o, p = O, i = K.i, s = K.s;
     if (i == s.length)
       s.push(value);
     return [
@@ -30,6 +36,10 @@ const {hooked, useRef, useState} = ((h, o, O, K) => ({
     ];
   }
 }))();
+
+const hooked = uhooks.hooked;
+const useRef = uhooks.useRef;
+const useState = uhooks.useState;
 
 exports.hooked = hooked;
 exports.useState = useState;
--- e.js
+++ e.js
@@ -3,26 +3,21 @@ const uhooks = ((h, o, O, K) => ({
   hooked: f => {
     const k = {i: 0, s: []};
     return function H() {
-      // const [p, r, e, v] = [h, o, O, K];
-      // [h, o, O, K] = [H, this, arguments, k];
       const p = h, r = o, e = O, v = K;
       h = H; o = this; O = arguments; K = k;
       K.i = 0;
       try { return f.apply(o, O); }
       finally {
         h = p; o = r; O = e; K = v;
-        // [h, o, O, K] = [p, r, e, v];
       }
     };
   },
   useRef: current => {
-    // const {i, s} = K;
     if (K.i == K.s.length)
       K.s.push({current: current});
     return K.s[K.i++];
   },
   useState: value => {
-    // const [y, u, p, {i, s}] = [h, o, O, K];
     const y = h, u = o, p = O, i = K.i, s = K.s;
     if (i == s.length)
       s.push(value);
@@ -39,5 +34,3 @@ const uhooks = ((h, o, O, K) => ({
 const hooked = uhooks.hooked;
 const useRef = uhooks.useRef;
 const useState = uhooks.useState;
-
-export {hooked, useState, useRef};
